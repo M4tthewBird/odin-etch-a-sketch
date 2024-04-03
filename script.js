916d5815f47ddfc998650aa26d320638bd8d29bf
Matthew Bird <ptacidlo@gmail.com>
2024-04-03 14:37:24 +0200

    Add function which created grid

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let lastValue = 16;
+
 function rangeSlide(value) {
-    document.getElementById('rangeValue').innerHTML = value;
+    document.getElementById('rangeValue').innerHTML = `${value}x${value}`;
+    console.log(value)
+    if (value != lastValue) {
+        createGrid(value);
+        lastValue = value;
+}   }
+
+function createGrid(gridSize) {
+    const container = document.querySelector('.grid-container');
+    container.style.setProperty('--rangeValue', gridSize); // Set CSS variable
+
+    container.innerHTML = '';
+
+    for (let y = 0; y < gridSize; y++) {
+        for (let x = 0; x < gridSize; x++) {
+            const div = document.createElement('div');
+            div.classList.add('grid-cell');
+            container.appendChild(div);
+        }
+    }
 }
